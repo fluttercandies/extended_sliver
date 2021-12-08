9f7da18eec962fa5f47a76d4f3aef1b35b177e4c
zmtzawqlp <zmtzawqlp@live.com>
2021-12-08 21:22:06 +0800

    update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:ff_annotation_route_library/ff_annotation_route_library.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'example_route.dart';
 import 'example_routes.dart';
@@ -17,27 +16,25 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: Routes.fluttercandiesMainpage,
-
-       onGenerateRoute: (RouteSettings settings) {
+      onGenerateRoute: (RouteSettings settings) {
         return onGenerateRoute(
           settings: settings,
           getRouteSettings: getRouteSettings,
           routeSettingsWrapper: (FFRouteSettings settings) {
-        if (settings.name == Routes.fluttercandiesMainpage ||
-              settings.name == Routes.fluttercandiesDemogrouppage ||
-              settings.name == Routes.fluttercandiesHomePage ||
-              settings.name == Routes.fluttercandiesSliverAppbar) {
-            return settings;
-          }
+            if (settings.name == Routes.fluttercandiesMainpage ||
+                settings.name == Routes.fluttercandiesDemogrouppage ||
+                settings.name == Routes.fluttercandiesHomePage ||
+                settings.name == Routes.fluttercandiesSliverAppbar) {
+              return settings;
+            }
             return settings.copyWith(
-                widget: CommonWidget(
-              child: settings.widget,
-              title: settings.routeName,
-            ));
+                builder: () => CommonWidget(
+                      child: settings.builder(),
+                      title: settings.routeName,
+                    ));
           },
         );
       },
-
     );
   }
 }
